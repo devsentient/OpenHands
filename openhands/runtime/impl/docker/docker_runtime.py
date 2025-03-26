149985f7e87521af9051350e9a4ae2dca4b7d6b8
tofarr <tofarr@gmail.com>
2025-03-26 07:37:10 -0600

    Replace use of requests with httpx (#7354)

--- openhands/runtime/impl/docker/docker_runtime.py
+++ openhands/runtime/impl/docker/docker_runtime.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 import os
 import docker
-import requests
+import httpx
 import tenacity
 from docker.models.containers import Container
 
@@ -348,9 +348,7 @@ class DockerRuntime(ActionExecutionClient):
 
     @tenacity.retry(
         stop=tenacity.stop_after_delay(120) | stop_if_should_exit(),
-        retry=tenacity.retry_if_exception_type(
-            (ConnectionError, requests.exceptions.ConnectionError)
-        ),
+        retry=tenacity.retry_if_exception_type((ConnectionError, httpx.NetworkError)),
         reraise=True,
         wait=tenacity.wait_fixed(2),
     )
